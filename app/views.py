230067c2948304dce6ef2edbadaab6aba1dea5ed
TotalWreck <ilai.barel@gmail.com>
2023-10-31 01:16:07 +0200

    jwt login 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -14,3 +15,9 @@ def register(request):
         username = request.data["username"],
         password = request.data["password"])
     return Response({"Authentication":"User created"})
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def auth(request):
+    print(request.user)
+    return Response({"Authentication":"Success"})
